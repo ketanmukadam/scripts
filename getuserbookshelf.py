e9ac261bf4395a10ebf9f34b09b9d172421f51ca
Ketan Mukadam <ketan.mukadam@broadcom.com>
2017-07-18 10:44:34 +0530

    Updated to scrapre for ISBN numbers

--- getuserbookshelf.py
+++ getuserbookshelf.py
@@ -57,8 +57,49 @@ def getpage(inurl, fname):
         soup = BS(driver.page_source,"lxml")
     return soup
 
+def scrapebookisbn(driver, bookname):
+    """
+    Scrape the ISBN number of the bookname 
 
-def scrapebookshelf(soup):
+    This function will search the bookname on goodreads and  
+    scrape the ISBN number
+    
+    Parameters
+    ---------
+    bookname : Name of book to search
+    
+    Returns
+    -------
+    None 
+    """
+    print("Searching ISBN for %s" % bookname)
+    driver.get("https://www.goodreads.com/")
+    try:
+       WebDriverWait(driver, 3).until(
+             EC.presence_of_element_located((By.ID, "sitesearch_field"))
+       )
+       driver.find_element_by_id("sitesearch_field").send_keys(bookname)
+       driver.find_element_by_id("sitesearch_field").send_keys(Keys.ENTER)
+       WebDriverWait(driver, 3).until(
+             EC.presence_of_element_located((By.LINK_TEXT, bookname))
+       )
+       driver.find_element_by_link_text(bookname).click()
+       WebDriverWait(driver, 5).until(
+             EC.presence_of_element_located((By.ID, "bookDataBox"))
+       )
+    except (NoSuchElementException, TimeoutException) :
+       print("No element found %s" % bookname)
+       return " "  
+    soup = BS(driver.page_source,"lxml")
+    elements = soup.findAll("div", {"class" : "clearFloats"})
+    for isbntags in elements:
+        isbnval = isbntags.findAll("div")
+        if isbnval[0].text.strip() == "ISBN": break
+        if isbnval[0].text.strip() == "ISBN13": break
+        if isbnval[0].text.strip() == "ASIN": break
+    return isbnval[1].text.split()[0]
+
+def scrapebookshelf(soup, skip_count, get_count):
     """
     Scrape all the books in the bookshelf 
 
@@ -75,16 +116,29 @@ def scrapebookshelf(soup):
     None 
     """
     booklist = list()
+    driver = webdriver.PhantomJS()
     for tableentry in soup.find_all("tbody",id="booksBody"):
         rows = tableentry.find_all("tr")
         for row in rows:
+            if skip_count:
+               skip_count -= 1
+               continue
+            if not get_count: break
             titlecols = row.find_all("td", attrs={"class" : "field title"})
             authorcols = row.find_all("td", attrs={"class" : "field author"})
             titlecols = [ele.find("div",attrs={"class" : "value"}).text.strip() for ele in titlecols if ele]
             authorcols = [ele.find("div",attrs={"class" : "value"}).text.strip() for ele in authorcols if ele]
             for t, a in zip(titlecols, authorcols):
-                booklist.append(t+"-"+a)
-    print(*booklist, sep='\n') 
+                isbn = scrapebookisbn(driver,t) 
+                booklist.append(t+"  "+a+"  "+isbn)
+            get_count -= 1
+    print(booklist)
+    try: 
+       ftxt = open('booklist.txt','w')
+       for b in booklist:
+           ftxt.write("%s\n" % b)
+    except (OSError, IOError) as e:
+       print(*booklist, sep='\n') 
     print("Found %d books in bookshelf" % len(booklist)) 
 
 def main(options):
@@ -100,7 +154,7 @@ def main(options):
     -------
     None
     """
-    scrapebookshelf(getpage("https://www.goodreads.com/review/list/"+options.user+"?shelf=%23ALL%23&view=table", options.user+".p"))
+    scrapebookshelf(getpage("https://www.goodreads.com/review/list/"+options.user+"?shelf=%23ALL%23&view=table", options.user+".p"), int(options.skip), int(options.count))
 
 def argparser():
     """
@@ -120,6 +174,10 @@ def argparser():
     parser = OptionParser(usage="usage: %prog [options]")
     parser.add_option(
         "-u","--username", dest="user", help="goodread username", default="44743670-sanjay-bakshi")
+    parser.add_option(
+        "-c","--count", dest="count", help="number of books to scrape", default="100")
+    parser.add_option(
+        "-s","--skip", dest="skip", help="number of books to skip", default="0")
     (opts, args) = parser.parse_args(sys.argv)
     return opts
 
